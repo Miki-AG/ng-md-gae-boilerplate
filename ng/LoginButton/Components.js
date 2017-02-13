184b12633db8bd38da3bd5bafa6d710e27db2194
System Administrator <miguel.a.garcia@ocecat.com>
2017-02-12 19:30:44 -0500

    fix

--- ng/LoginButton/Components.js
+++ ng/LoginButton/Components.js
@@ -2,50 +2,85 @@ angular.module('project')
     .directive('signUp', function() {
         return {
             restrict: 'E',
-            controller: 'LoginCtrl',
+            controller: 'LoginButtonCtrl',
             templateUrl: 'ng/LoginButton/button.tpl.html',
             scope: {
                 type: '='
             }
         }
     })
-    .controller('LoginCtrl', function($scope, $state, $firebaseAuth, $mdDialog, CONFIG) {
+    .controller('LoginButtonCtrl', function($scope, $state, $mdDialog, CONFIG) {
         $scope.CONFIG = CONFIG;
         $scope.state = $state;
-        var auth = $firebaseAuth();
 
-        $scope.pickLoginProviderDialog = function(ev) {
+        $scope.pickLoginProviderDialog = function(type) {
+            console.log(type);
             $mdDialog.show({
-                    controller: 'HeaderCtrl',
-                    templateUrl: 'ng/LoginButton/login.tpl.html',
-                    parent: angular.element(document.body),
-                    targetEvent: ev,
-                    clickOutsideToClose: true
-                })
-                .then(function(answer) {
-                    $scope.status = 'You said the information was "' + answer + '".';
-                }, function() {
-                    $scope.status = 'You cancelled the dialog.';
+                locals: { typeOfDialog: type }, //here where we pass our data
+                controller: 'LoginDialogCtrl',
+                templateUrl: 'ng/LoginButton/login.tpl.html',
+                parent: angular.element(document.body),
+                clickOutsideToClose: true
+            });
+        }
+    })
+    .controller('LoginDialogCtrl', function($scope, $state, $firebaseAuth, $mdDialog, CONFIG, $mdToast, typeOfDialog) {
+        $scope.CONFIG = CONFIG;
+        $scope.state = $state;
+        $scope.auth = $firebaseAuth();
+        $scope.type = typeOfDialog;
+        $scope.formData = {};
+
+        $scope.pickLoginProviderDialog = function(type) {
+            console.log(type);
+            $mdDialog.show({
+                controller: 'LoginCtrl',
+                templateUrl: 'ng/LoginButton/login.tpl.html',
+                parent: angular.element(document.body),
+                targetEvent: ev,
+                clickOutsideToClose: true
+            });
+        }
+
+        $scope.loginProvider = function(provider) {
+            $scope.auth.$signInWithPopup(provider).then(function(firebaseUser) {
+                $mdToast.show(
+                    $mdToast.simple()
+                    .textContent('Signed in using ' + provider)
+                    .position('bottom left')
+                    .hideDelay(3000)
+                );
+                $mdDialog.hide();
+            }).catch(function(error) {
+                $mdToast.show(
+                    $mdToast.simple()
+                    .textContent('Authentication failed: ' + error)
+                    .position('bottom left')
+                    .hideDelay(3000)
+                );
+            });
+        }
+        $scope.loginEmail = function() {
+            $scope.auth.$signInWithEmailAndPassword(
+                    $scope.formData.email,
+                    $scope.formData.password)
+                .then(function(firebaseUser) {
+                    $mdToast.show(
+                        $mdToast.simple()
+                        .textContent('Signed in')
+                        .position('bottom left')
+                    );
+                    $mdDialog.hide();
+                }).catch(function(error) {
+                    $mdToast.show(
+                        $mdToast.simple()
+                        .textContent('Authentication failed: ' + error)
+                        .position('bottom left')
+                        .hideDelay(3000)
+                    );
                 });
         }
 
-        $scope.loginEmail = function() {
-            console.log('in login');
-            auth.$signInWithPopup("facebook").then(function(firebaseUser) {
-                console.log("Signed in as:", firebaseUser.user.uid);
-            }).catch(function(error) {
-                console.log("Authentication failed:", error);
-            });
-        }
-
-        $scope.loginFacebook = function() {
-            console.log('in login');
-            auth.$signInWithPopup("facebook").then(function(firebaseUser) {
-                console.log("Signed in as:", firebaseUser.user.uid);
-            }).catch(function(error) {
-                console.log("Authentication failed:", error);
-            });
-        }
         $scope.logout = function() {
             console.log('in login');
             auth.$signOut();
@@ -57,5 +92,8 @@ angular.module('project')
             } else {
                 console.log("Signed out");
             }
+        }
+        $scope.closeDialog = function() {
+            $mdDialog.hide();
         }
     });
