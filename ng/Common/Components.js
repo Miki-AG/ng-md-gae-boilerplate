d7b2bd21526254797c35aa5a9a4775841dca9575
System Administrator <miguel.a.garcia@ocecat.com>
2017-05-18 10:43:00 -0400

    fix

--- ng/Common/Components.js
+++ ng/Common/Components.js
@@ -3,4 +3,21 @@
             'LOGIN': 0,
             'REGISTER': 1,
             'LOGOUT': 1
+        })
+        .filter("filterIsImage", function() {
+            return function(dates, month) {
+                return dates.filter(function(item) {
+                    return item.indexOf(month) > -1;
+                });
+            };
+        })
+        .filter("isImage", function() {
+            return function(filename) {
+                var ext = filename.split('.').pop();
+                if (ext.match(/(jpg|jpeg|png|gif)$/i)) {
+                    return true
+                } else {
+                    return false
+                };
+            };
         });
